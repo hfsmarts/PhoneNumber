df2b2f84e35d9c65b9a762d7e75bbb837e57eebf
hfsmarts <harunfazlic@yahoo.com>
2023-05-10 07:11:46 +0200

    Initial commit

--- PhoneNumber/ViewController.swift
+++ PhoneNumber/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import PhoneNumberKit
 
 class ViewController: UIViewController {
+    
+    let phoneNumberKit = PhoneNumberKit()
 
+    @IBOutlet var numberText: UITextField!
+    @IBOutlet var numberValidationResult: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func buttonIsCliked(_ sender: UIButton) {
+        do {
+            let phoneNumber = try phoneNumberKit.parse(numberText.text ?? "")
+            numberValidationResult.text = "Phone number is valid"
+            numberValidationResult.textColor = .green
+        } catch {
+            numberValidationResult.text = "Phone number is not valid"
+            numberValidationResult.textColor = .red
+        }
+    }
+    
 
 }
 
---
+++ Pods/Target Support Files/Pods-PhoneNumber/Pods-PhoneNumber-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhoneNumber : NSObject
+@end
+@implementation PodsDummy_Pods_PhoneNumber
+@end
